1aa9f5603547d8f9b5fd19826b25e220536b1b93
github_username <you@example.com>
2023-04-27 19:59:35 -0400

    challenege04 commit

---
+++ Challenge04.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Script Name:                  Script Name
+# Author:                       NATASHA SIRAMARCO
+# Date of latest revision:      00/00/0000
+# Purpose:                      Purpose
+
+# Declaration of variables
+# Creates four directories: dir1, dir2, dir3, dir4
+dirs=("dir1" "dir2" "dir3" "dir4")
+
+
+# Declaration of functions
+
+
+# Main
+
+# Put the names of the four directories in an array
+echo "Directory names: ${dirs[@]}"
+
+# References the array variable to create a new .txt file in each directory
+
+# End
