fbb3c98e6b79e73923e91c782ed8382ff9541993
github_username <you@example.com>
2023-05-01 13:29:51 -0400

    ch05

--- Ch05.sh
+++ Ch05.sh
@@ -32,11 +32,3 @@ done
 
 
 # End
-
-
-# The script uses a while loop to ensure that the script repeats until the user exits the script by pressing Ctrl + C. 
-# Inside the loop, the script uses the ps aux command to display the running processes, then prompts the user to enter a 
-# PID to kill. If the user enters a valid PID, the script uses the kill command with the -9 option to send a SIGKILL signal (signal number 9) 
-# to the process with that PID, which terminates the process. If the PID is not valid or the process cannot be killed 
-# for some reason, the script prints an error message.
-# The loop ensures that the script repeats until the user exits by pressing Ctrl + C.
