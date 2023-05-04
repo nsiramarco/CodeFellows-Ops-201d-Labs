4d76f27a6188348be1ed2edf5344f27f8fb94665
github_username <you@example.com>
2023-05-04 13:33:42 -0400

    Challenge 08

--- CH08.bat
+++ CH08.bat
@@ -1,11 +1,1 @@
-@echo off
-
-set "source=C:\Users\Jorge\Desktop\Work"
-set "destination=D:\Backup\Work"
-
-echo Copying files from %source% to %destination%...
-
-robocopy "%source%" "%destination%" /E /R:0 /W:0 /MT:32 /XJ
-
-echo Copy completed.
-pause
+robocopy C:\Users\j.thompson\Desktop C:\Users\j.thompson\Documents /log+:jthompson.txt
