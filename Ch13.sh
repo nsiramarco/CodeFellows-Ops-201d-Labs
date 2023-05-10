b51c498476c3c6fd01bb2e481105e370d096066e
github_username <you@example.com>
2023-05-10 17:47:08 -0400

    Challenge 13

---
+++ Ch13.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Script Name:                  Challenge 13
+# Author:                       NATASHA SIRAMARCO
+# Date of latest revision:      05/10/2023
+# Purpose:                      Create a script that asks a user to type a domain, then displays information about the typed domain. 
+
+
+
+
+# Install whois
+sudo apt-get update
+sudo apt-get -y install whois
+
+# Declaration of variables
+whois $domain
+
+# Take a user input string. Presumably the string is a domain name such as Google.com.
+read -p "Enter Webpage Name: " domain
+cd /home/ns/CodeFellows-Ops-201d-Labs/CodeFellows-Ops-201d-Labs-2/
+touch domaininfo.txt
+
+# Declaration of functions
+Information_Gather() {
+
+# Run whois against a user input string.
+whois $domain > domaininfo.txt
+# Run dig against the user input string.
+dig $domain >> domaininfo.txt
+# Run host against the user input string.
+host $domain >> domaininfo.txt
+# Run nslookup against the user input string.
+nslookup $domain >> domaininfo.txt
+
+}
+
+# Main
+# Open Text Editor
+Information_Gather > domaininfo.txt
+
+# End
